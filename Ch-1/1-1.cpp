b1dc8fcc55240704e05ac3da68f3e4aa8dd1ed34
IamJayGondaliya <prof.rnwjay2205@gmail.com>
2023-04-11 12:46:45 +0530

    Ch. 1: Intrduction of C++

---
+++ Ch-1/1-1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	
+	cout << "Enter a: ";
+	cin  >> a;
+	cout << "Enter b: ";
+	cin  >> b;
+	 
+	cout << "a: " << a << endl << "b: " << b;
+}
+
+/*
+		Escape sequence characters		=> \n, \t
+		Manipulators					=> endl
+*/
